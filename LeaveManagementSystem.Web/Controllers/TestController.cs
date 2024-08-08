11ebff168eeaff2572861bba7ac0430d8acb3a32
Deepaktechnicalworld <Deepaktechnicalworld@LAPTOP-M76GOGVA>
2024-08-08 09:04:48 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeaveManagementSystem.Web.Models;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateOfBirth = new DateTime(1999,12,07)
+            };
+
+            return View(data);
+        }
+    }
+}
